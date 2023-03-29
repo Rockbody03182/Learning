3168ddafabf7cd12b9bb2b8a83a934773ab999f5
Jade <rockbody03182@gmail.com>
2023-03-30 07:42:51 +0900

    modify js directory

---
+++ javascript/grammar/variable.js
@@ -0,0 +1,19 @@
+/**
+ * 변수 Variable
+ * 변수는 위치(주소)를 기억하는 저장소이다.
+ * 위치란 메모리 상의 주소(address)를 의미한다.
+ * 즉, 변수란 메모리 주소(Memory address)에 접근하기 위해 사람이 이해할 수 있는 언어로 지정한 식별자(identifier)이다.
+ *
+ * 데이타 타입 Data type
+ *  원시 타입 (primitive data type)
+ *    number
+ *    string
+ *    boolean
+ *    null
+ *    undefinded
+ *    symbol (New in ECMAScript 6)
+ *  객체 타입 (Object data type)
+ *    object
+ *
+ *
+ */
